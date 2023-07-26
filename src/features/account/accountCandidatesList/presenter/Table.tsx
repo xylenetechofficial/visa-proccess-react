75025cd40995385705832e65bbeae81cad22c63c
Neerajsahni <neerajsahni367@gmail.com>
2023-07-26 22:03:44 +0530

    AccoutnCandidateList Frontend

---
+++ src/features/account/accountCandidatesList/presenter/Table.tsx
@@ -0,0 +1,73 @@
+
+import {
+  Table,
+  TableBody2,
+  TableCell,
+  TableHead2,
+  TableHeadCell,
+  TableHeadRow,
+  TableRow,
+
+} from "../../../../componenets/Table";
+import { Checkbox } from "@mui/material";
+import { DateInput, UnlabeledInput } from "../../../../componenets/Input";
+import { CustomSelectComponentUnlabeledv2, selectOptionConveterv2 } from "../../../../componenets/SelectBox";
+import { useState } from "react";
+import { convertDateFormat } from "../../../../utils/function";
+
+const CandidatesListTable = (props: {
+
+  candidatesList: any;
+  setCandidatesList: any
+  data: any;
+  setData: any;
+}) => {
+
+  const [date, setDate] = useState<any>([])
+  console.log(props.data)
+  return (
+    <div className="overflow-auto">
+
+      <Table>
+        <TableHead2>
+          <TableHeadRow>
+            <TableHeadCell> Sr No.</TableHeadCell>
+            <TableHeadCell> CANDIDATE NO.</TableHeadCell>
+            <TableHeadCell> PARTY CODE </TableHeadCell>
+            <TableHeadCell> COMPANY NAME</TableHeadCell>
+            <TableHeadCell> CANDIDATE NAME</TableHeadCell>
+            <TableHeadCell> PASSPORT NO.</TableHeadCell>
+            <TableHeadCell> ACTUAL PROFESSION </TableHeadCell>
+            <TableHeadCell> VISA PROFESSION </TableHeadCell>
+            <TableHeadCell> AGENT</TableHeadCell>
+            <TableHeadCell> VISA RECIEVED DATE </TableHeadCell>
+            <TableHeadCell> PROCESS CHARGES </TableHeadCell>
+            <TableHeadCell> DOCUMENT CHARGES</TableHeadCell>
+            <TableHeadCell> OTHER CHARGES </TableHeadCell>
+            <TableHeadCell> SECTOR CHARGES</TableHeadCell>
+            <TableHeadCell> PARTIAL CHARGES </TableHeadCell>
+            <TableHeadCell> SERVICE CHARGES </TableHeadCell>
+          </TableHeadRow>
+        </TableHead2>
+        <TableBody2>
+
+          <TableRow>
+
+            <TableCell> </TableCell>
+            <TableCell> </TableCell>
+            <TableCell> </TableCell>
+
+            
+
+          </TableRow>
+
+
+
+        </TableBody2>
+      </Table>
+
+    </div>
+  );
+};
+
+export default CandidatesListTable;
--- src/navigation.ts
+++ src/navigation.ts
@@ -126,6 +126,7 @@ export const navigations: NavigationInterface[] = [
       { name: 'Agent Bulk Payments', icon: 'pageview', path: '/account/agent-bulk-payment' },
       { name: 'Incentives', icon: 'pageview', path: '/account/incentives' },
       { name: 'Delhi/Other Daily Payments', icon: 'pageview', path: '/account/delhi-other-daily-payments' },
+      { name: 'Account Candidate List', icon: 'preview', path:'/account/account-candidate-list'},
     ]
   },
   {
