cd502a24348bcffe028d4fe935f9acddf0b25d60
neerajsahni <neerajsahni367@gmail.com>
2023-07-29 17:09:28 +0530

    payment modal

--- src/features/account/agentPayment/presenter/PaymentBulkList.tsx
+++ src/features/account/agentPayment/presenter/PaymentBulkList.tsx
@@ -2,8 +2,11 @@
 import {  Table2, TableBody,  TableCell,  TableCell3,  TableHead2,  TableHeadCell3,  TableHeadRow2, TableRow, TableRow1 } from '../../../../componenets/Table';
 import { convertDateFormat } from '../../../../utils/function';
 
-const PaymentBulkList = (AgentPaymentList:any) => {
-    console.log(AgentPaymentList?.AgentPaymentList?.bulk_payment_list,"jpaymentj")
+const PaymentBulkList = (props:{
+    AgentPaymentList:any,
+    setModalName:any
+}) => {
+    console.log(props?.AgentPaymentList?.bulk_payment_list,"jpaymentj")
     return (
 
         <div className='overflow-auto shadow-md shadow-slate-500 rounded-lg justify-center h-80'>
@@ -22,12 +25,12 @@ const PaymentBulkList = (AgentPaymentList:any) => {
                     </TableHeadRow2>
                 </TableHead2>
                 <TableBody>
-                    {AgentPaymentList?.AgentPaymentList?.bulk_payment_list?.map((ele :any, index:any)=>(
+                    {props?.AgentPaymentList?.bulk_payment_list?.map((ele :any, index:any)=>(
                          <TableRow1 key={index} onClick={()=>console.log(ele)}> 
                         <TableCell3 width={10}>{index+1}</TableCell3>
                         <TableCell3 width={10}>{convertDateFormat(ele.created_at)}</TableCell3>
                         <TableCell3 width={10}>{ele.amount}</TableCell3>
-                        <TableCell3 width={10}>{ele.used_amount}</TableCell3>
+                        <TableCell3 width={10}><p onClick={()=>props.setModalName('viewbulkpayment')}>{ele.used_amount}</p></TableCell3>
                         <TableCell3 width={10}>{ele.description}</TableCell3>
                         </TableRow1>
                     ))}
---
+++ src/features/account/agentPayment/presenter/PaymentDetailFromBulk.tsx
@@ -0,0 +1,119 @@
+import { SubHeading1, UpdateContentBox } from "../../../../componenets/CoustomHeader"
+import { DateInput } from "../../../../componenets/Input"
+import { FullScreenModal } from "../../../../componenets/Modal"
+import { CustomSelectComponentUnlabeled } from "../../../../componenets/SelectBox"
+import PaymentDetailBulkTable from "./PaymentDetailBulkTable"
+import { Box } from "@mui/material";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 900,
+    bgcolor: 'background.paper',
+    border: '2px solid #fff',
+    boxShadow: 24,
+    borderRadius: 2,
+    p: 4,
+};
+export default function Main (props:
+    {
+        onClose:any,
+
+}){
+const onClickAdd =()=>{
+
+}
+
+    return (
+        <>
+{/*         
+        <FullScreenModal
+            buttonName="Update"
+            handleClick={onClickAdd}
+            title="Update Block Visa"
+            onClose={props.onClose}
+        > */}
+
+<Box sx={style}>
+            <h3 className="mb-4 text-2xl font-medium text-gray-900 dark:text-white">Payment Detail</h3>
+            <button
+                type="button"
+                className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                onClick={() => props.onClose()}
+            >
+                <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                    ></path>
+                </svg>
+                <span className="sr-only">Close modal</span>
+            </button>
+            
+            <div className="grid grid-cols-1 py-3  gap-2 shadow">
+                <UpdateContentBox>
+
+                    <SubHeading1 text="AGENT NAME  :" />
+                    <DateInput
+                        id="sd;fksdakj"
+                        value={"AgentPayment.index_date"}
+                        // onChange={(value) => setAgentPayment({ ...AgentPayment, index_date: value }
+                        onChange={()=>console.log("ss")}
+                            
+                    
+                    />
+                </UpdateContentBox>
+                <UpdateContentBox>
+
+                    <SubHeading1 text="BULK PAYMENT AMOUNT  :" />
+                    <DateInput
+                        id="sd;fksdakj"
+                        value={"AgentPayment.index_date"}
+                        // onChange={(value) => setAgentPayment({ ...AgentPayment, index_date: value }
+                        onChange={()=>console.log("ss")}
+                            
+                    
+                    />
+                </UpdateContentBox>
+                <UpdateContentBox>
+
+                    <SubHeading1 text="BULK PAYMENT DETAILS  :" />
+                    <DateInput
+                        id="sd;fksdakj"
+                        value={"AgentPayment.index_date"}
+                        // onChange={(value) => setAgentPayment({ ...AgentPayment, index_date: value }
+                        onChange={()=>console.log("ss")}
+                            
+                    
+                    />
+                </UpdateContentBox>
+                <UpdateContentBox>
+
+                    <SubHeading1 text="BULK PAYMENT DATE  :" />
+                    
+                </UpdateContentBox>
+
+
+           </div>
+
+
+            <PaymentDetailBulkTable
+                visaProfessionList={[]}
+                onChange={(value) => console.log("Df")}
+            />
+
+        </Box>
+    )
+        
+        </>
+    )
+}
