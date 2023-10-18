2e8f0f863e7e1c843ea21be26a9c35a22c37b9ca
AmitMali <amitmai978@gmail.com>
2023-10-18 21:11:23 +0530

    converter by Amit

--- src/features/visa/indexFull/type/index.ts
+++ src/features/visa/indexFull/type/index.ts
@@ -1,10 +1,4 @@
-// export interface FullIndexInterface{
-//     id?:number
-//     job_order_no:string
-//     	index_date	company name	party code	quantity	used qty	dead visa qty	expired visa qty	cancelled qty	balance qty	visa issue date	visa expiry date	country	visa date (arabic)	visa number	visa fee	visa authorization	visa submission	sponsor id	aravic sponsor name	division	number of days left for visa expiry	mofa done	pp submission	visa cancel	view visa prof.	visa prof. edit	edit
-// }
+import * as indexVisa from './IndexVisa'
+import * as visaProfession from './VisaProfession'
 
-
-// interface 
-// adapter
-// converter class
+export visaProfession.FullIndexViewConverter as FullIndexViewConverter 
